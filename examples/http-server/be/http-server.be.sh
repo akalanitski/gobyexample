47f45d45efed45823b9e954e73197bf0faa94e66
akalanitski <akalanitski@gmail.com>
2025-07-13 19:40:55 +0200

    Change structure of directories to have all languages always. Add itaien translations

---
+++ examples/http-server/be/http-server.be.sh
@@ -0,0 +1,6 @@
+# Запусціць сервер у фонавым рэжыме.
+$ go run http-server.go &
+
+# Доступ да маршруту `/hello`.
+$ curl localhost:8090/hello
+hello
---
+++ examples/number-parsing/be/number-parsing.sh
@@ -0,0 +1,9 @@
+$ go run number-parsing.go 
+1.234
+123
+456
+789
+135
+strconv.ParseInt: parsing "wat": invalid syntax
+
+# Далей мы разгледзім яшчэ адну распаўсюджаную задачу парсінгу: URL-адрасы.
