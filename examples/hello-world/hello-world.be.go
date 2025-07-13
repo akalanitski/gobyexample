b82bce666f78cb8271cd93df2c3bcfeaf339b62b
akalanitski <akalanitski@gmail.com>
2025-07-13 19:39:23 +0200

    Change structure of directories to have all languages always. Add itaien translations

--- examples/hello-world/hello-world.be.go
+++
@@ -1,9 +0,0 @@
-// Наша першая праграма вывядзе паведамленне "hello world" на экран
-// Вось поўны зыходны код.
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("hello world")
-}
--- examples/pointers/pointers.be.sh
+++
@@ -1,8 +0,0 @@
-# `zeroval` не змяняе `i` у `main`, але
-# `zeroptr` змяняе, бо мае спасылку на
-# адрас памяці для гэтай зменнай.
-$ go run pointers.go
-initial: 1
-zeroval: 1
-zeroptr: 0
-pointer: 0x42131100
