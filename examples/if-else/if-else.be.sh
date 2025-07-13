1e433b300f7f80aa7f6a24377132521676f77d9c
akalanitski <akalanitski@gmail.com>
2025-07-13 19:39:23 +0200

    Change structure of directories to have all languages always. Add itaien translations

--- examples/if-else/if-else.be.sh
+++
@@ -1,9 +0,0 @@
-$ go run if-else.go
-7 is odd
-8 is divisible by 4
-either 8 or 7 are even
-9 has 1 digit
-
-# У Go няма [тернарннага аператар ?:](https://en.wikipedia.org/wiki/%3F:)
-# таму вам трэба будзе выкарыстоўваць поўны канструкцыю `if` нават
-# для простых умоў.
